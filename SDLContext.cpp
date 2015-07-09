7c409a852fd3aeb2f6dcfa702e1eb9cfea17e449
Michal Mieczkowski <m.mieczkowsk@samsung.com>
2015-07-09 16:58:38 +0200

    Some cosmetic changes.

--- SDLContext.cpp
+++ SDLContext.cpp
@@ -1,40 +1,1 @@
-//
-// Created by mhl on 08.07.15.
-//
-
-#include "SDLContext.h"
-
-
-SDL_Window *SDLContext::createWindow(int pos_x, int pos_y, int width, int height, const std::string title) {
-    return SDL_CreateWindow(title.c_str(), pos_x, pos_y, width, height, SDL_WINDOW_SHOWN);
-}
-
-SDL_Renderer *SDLContext::createRenderer(SDL_Window *win, int index, uint32_t flags) {
-    return SDL_CreateRenderer(win, index, flags);
-}
-
-SDL_Texture *SDLContext::loadTexture(const std::string &filePath, SDL_Renderer *renderer) {
-    SDL_Texture *texture = nullptr;
-    SDL_Surface *surf = SDL_LoadBMP(filePath.c_str());
-    if(surf != nullptr) {
-        texture = SDL_CreateTextureFromSurface(renderer, surf);
-        SDL_FreeSurface(surf);
-        if(texture == nullptr) {
-            return nullptr;
-        }
-    }
-    else {
-        return nullptr;
-    }
-    return texture;
-}
-
-void SDLContext::renderTexture(SDL_Renderer *renderer, SDL_Texture *texture, int x, int y, int scale) {
-    SDL_Rect dst;
-    dst.x = x;
-    dst.y = y;
-    SDL_QueryTexture(texture, NULL, NULL, &dst.w, &dst.h);
-    dst.x = dst.x / scale;
-    dst.y = dst.y / scale;
-    SDL_RenderCopy(renderer, texture, NULL, &dst);
-}
+//// Created by mhl on 08.07.15.//#include "SDLContext.h"SDL_Window *SDLContext::createWindow(int pos_x, int pos_y, int width, int height, const std::string title) {    return SDL_CreateWindow(title.c_str(), pos_x, pos_y, width, height, SDL_WINDOW_SHOWN);}SDL_Renderer *SDLContext::createRenderer(SDL_Window *win, int index, uint32_t flags) {    return SDL_CreateRenderer(win, index, flags);}SDL_Texture *SDLContext::loadTexture(const std::string &filePath, SDL_Renderer *renderer) {    SDL_Texture *texture = nullptr;    SDL_Surface *surf = SDL_LoadBMP(filePath.c_str());    if (surf != nullptr) {        texture = SDL_CreateTextureFromSurface(renderer, surf);        SDL_FreeSurface(surf);        if (texture == nullptr) {            return nullptr;        }    }    else {        return nullptr;    }    return texture;}void SDLContext::renderTexture(SDL_Renderer *renderer, SDL_Texture *texture, int x, int y) {    SDL_Rect dst;    dst.x = x;    dst.y = y;    SDL_QueryTexture(texture, nullptr, nullptr, &dst.w, &dst.h);    SDL_RenderCopy(renderer, texture, nullptr, &dst);}template<typename T>void SDLContext::releaseResource(T *res) {    releaseResource(res);}template<>void SDLContext::releaseResource<SDL_Window>(SDL_Window *res) {    std::cout << "Destroying: " << res << " window" << std::endl;    SDL_DestroyWindow(res);}template<>void SDLContext::releaseResource<SDL_Renderer>(SDL_Renderer *res) {    std::cout << "Destroying: " << res << " renderer" << std::endl;    SDL_DestroyRenderer(res);}template<>void SDLContext::releaseResource<SDL_Texture>(SDL_Texture *res) {    std::cout << "Destroying: " << res << " texture" << std::endl;    SDL_DestroyTexture(res);}int SDLContext::clearRenderer(SDL_Renderer *renderer) {    return SDL_RenderClear(renderer);}void SDLContext::sleep(unsigned int ms) {    SDL_Delay(ms);}void SDLContext::render(SDL_Renderer *render) {    SDL_RenderPresent(render);}
--- main.cpp
+++ main.cpp
@@ -1,25 +1,1 @@
-#include <iostream>
-#include <SDL2/SDL.h>
-#include "SDLContext.h"
-
-
-int main(int argc, char *argv[]) {
-    SDLContext sdl;
-    SDL_Window *mainWindow = sdl.createWindow(100,100,1024,768, "Hello SDL");
-    SDL_Renderer *renderer = sdl.createRenderer(mainWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if(renderer == nullptr)
-    	return -1;
-    SDL_Texture *texture = sdl.loadTexture("assets/marbles.bmp", renderer);
-    if(texture == nullptr) {
-    	return -1;
-    }
-    sdl.renderTexture(renderer, texture, 10, 60, 10);
-
-    for(int i = 0; i < 3; ++i) {
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture, nullptr, nullptr);
-        SDL_RenderPresent(renderer);
-        SDL_Delay(1000);
-    }
-    return 0;
-}
+#include <iostream>#include <SDL2/SDL.h>#include "SDLContext.h"int main(int argc, char *argv[]) {    SDLContext sdl;    SDL_Window *mainWindow = sdl.createWindow(100, 100, 1024, 768, "Hello SDL");    SDL_Renderer *renderer = sdl.createRenderer(mainWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);    if (renderer == nullptr) {        std::cerr << "Can't create window!" << std::endl;        return -1;    }    /*     * TODO: Fix path, and resource discovery     */    SDL_Texture *texture = sdl.loadTexture("../../assets/marbles.bmp", renderer);    if (texture == nullptr) {        std::cerr << "Can't load texture! Not such file or directory?" << std::endl;        return -1;    }    sdl.clearRenderer(renderer);    sdl.renderTexture(renderer, texture, 0, 50);    sdl.renderTexture(renderer, texture, 50, 0);    sdl.render(renderer);    sdl.sleep(1500);    sdl.releaseResource(texture);    sdl.releaseResource(renderer);    sdl.releaseResource(mainWindow);    return 0;}
